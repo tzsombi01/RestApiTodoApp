f9f5ea1461d92180c62e5095f0ad4f23ab84f6b9
tzsombi01 <toreky.zsombor@gmail.com>
2022-12-23 20:26:49 +0100

    Initial Commit

---
+++ src/main/java/com/tzsombi/RestApiTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.tzsombi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTodoAppApplication.class, args);
+	}
+
+}
